8868893213a8844e7f4380dc72bf7030fce08429
Fumiaki Yoshimatsu <fumiaki@madolabs.co>
2019-04-18 10:23:41 -0400

    Initial Commit

---
+++ HelloSketch/HelloSketch.h
@@ -0,0 +1,19 @@
+//
+//  HelloSketch.h
+//  HelloSketch
+//
+//  Created by Fumiaki Yoshimatsu on 4/18/19.
+//  Copyright © 2019 Fumiaki Yoshimatsu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HelloSketch.
+FOUNDATION_EXPORT double HelloSketchVersionNumber;
+
+//! Project version string for HelloSketch.
+FOUNDATION_EXPORT const unsigned char HelloSketchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloSketch/PublicHeader.h>
+
+
